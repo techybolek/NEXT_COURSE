75ae21882b47e0bc13d100cb5ccce2bfbf8b5275
techybolek <techy_bolek@yahoo.com>
2025-06-29 11:05:30 -0600

    addpost works

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,48 +1,7 @@
-import { useState, useEffect } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
-function PostList() {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(true);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      try {
-        setIsFetching(true);
-        const response = await fetch("http://localhost:8080/posts");
-        const data = await response.json();
-        setPosts(data.posts || []);
-        setIsFetching(false);
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-        setIsFetching(false);
-      }
-    }
-    fetchPosts();
-  }, []);
-
-  const addPostHandler = async (newPost) => {
-    try {
-      const response = await fetch("http://localhost:8080/posts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newPost),
-      });
-      const data = await response.json();
-
-      setPosts((prevPosts) => [
-        { id: prevPosts.length + 1, ...newPost },
-        ...prevPosts,
-      ]);
-    } catch (error) {
-      console.error("Error adding post:", error);
-      throw error;
-    }
-  };
-
+function PostList({ posts, isFetching }) {
   return (
     <>
       {isFetching && <p>Loading posts...</p>}
--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -1,13 +1,54 @@
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import PostList from "../components/PostList";
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        setIsFetching(true);
+        const response = await fetch("http://localhost:8080/posts");
+        const data = await response.json();
+        setPosts(data.posts || []);
+        setIsFetching(false);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        setIsFetching(false);
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  const addPostHandler = async (newPost) => {
+    try {
+      const response = await fetch("http://localhost:8080/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPost),
+      });
+      const data = await response.json();
+
+      setPosts((prevPosts) => [
+        { id: prevPosts.length + 1, ...newPost },
+        ...prevPosts,
+      ]);
+    } catch (error) {
+      console.error("Error adding post:", error);
+      throw error;
+    }
+  };
+
   return (
     <>
       <main>
-        <PostList />
+        <PostList posts={posts} isFetching={isFetching} />
       </main>
-      <Outlet />
+      <Outlet context={{ addPostHandler }} />
     </>
   );
 }
